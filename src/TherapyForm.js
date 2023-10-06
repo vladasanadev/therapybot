a730901eec806ee5449d1b1018557d097403ad87
Vlada <vlada@Vladas-MacBook-Pro.local>
2023-10-06 17:53:05 -0400

    update styles

--- src/TherapyForm.js
+++ src/TherapyForm.js
@@ -6,13 +6,17 @@ function TherapyForm(props) {
     return (
         <div className="container d-flex justify-content-center align-items-center vh-100">
         <div className="form-container">
-          <h1 className="text-center first-color-text custom-font">Hey</h1>
-          <h2 className="text-center second-color-text custom-font">specify your request</h2>
+        <div className="text-center">
+        <p className="first-color-text custom-font uppercase-text">
+                        <i className="fas fa-comment"></i> Hey
+                    </p>
+                    </div>
+          <h3 className="text-center second-color-text custom-font uppercase-text">specify your request</h3>
           <form className="text-center">
             <div className="form-group">
               <textarea className="form-control first-color" id="exampleFormControlTextarea1" rows="6"></textarea>
             </div>
-            <button type="submit" className="btn btn-primary first-color custom-font">Submit</button>
+            <button type="submit" className="btn btn-primary third-color first-color-text custom-font uppercase-text">Submit</button>
           </form>
         </div>
       </div>
