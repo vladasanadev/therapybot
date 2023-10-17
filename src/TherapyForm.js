bc54cb2a29c6afa3eb025bd41a965e54dbb4520b
Vlada <hello.vladasana@gmail.com>
2023-10-16 22:27:55 -0400

    added connection to back

--- src/TherapyForm.js
+++ src/TherapyForm.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import axios from 'axios';
 
 function TherapyForm(props) {
+  const sendMessage = async (message) => {
+    try {
+      const response = await axios.post('YOUR_BACKEND_URL/api/chat', { message });
+      const reply = response.data.reply;
+      console.log(response.data.reply)
+      // Handle the reply (display it, etc.)
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
     return (
         <div className="container d-flex justify-content-center align-items-center vh-100">
         <div className="form-container">
@@ -16,7 +27,7 @@ function TherapyForm(props) {
             <div className="form-group">
               <textarea className="form-control first-color" id="exampleFormControlTextarea1" rows="6"></textarea>
             </div>
-            <button type="submit" className="btn btn-primary third-color first-color-text custom-font uppercase-text">Submit</button>
+            <button type="submit" className="btn btn-primary third-color first-color-text custom-font uppercase-text" onClick={sendMessage}>Submit</button>
           </form>
         </div>
       </div>
